c9d16ea2bf36894f72c1bf9853d0d0df4f52c9a7
paulsuarez356 <paul.suarez@mrsmoysclass.com>
2025-05-23 14:53:31 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,14 +1,40 @@
-import javax.swing.*;
-
+/**
+ * Abstract base class for all casino games.
+ * <p>
+ * The Game class provides a common structure for casino-style games and manages
+ * the player and a deck of cards. Subclasses must implement the play method to define
+ * their game logic.
+ */
 public abstract class Game {
-    protected final Player player;
-    protected final Deck deck;
 
+    /**
+     * The player participating in the game.
+     */
+    protected Player player;
+
+    /**
+     * The deck of cards used in the game.
+     */
+    protected Deck deck;
+
+    /**
+     * Constructs a new Game with the specified player.
+     * Initializes a new shuffled deck for the game.
+     *
+     * @param player the Player participating in the game
+     */
     public Game(Player player) {
         this.player = player;
         this.deck = new Deck();
-        this.deck.shuffle();
+        deck.shuffle();
     }
 
-    public abstract void play(JFrame frame);
+    /**
+     * Starts and manages a round of the game.
+     * <p>
+     * Subclasses must implement this method to provide the specific game logic.
+     *
+     * @param frame the JFrame used for displaying dialogs and messages
+     */
+    public abstract void play(javax.swing.JFrame frame);
 }
